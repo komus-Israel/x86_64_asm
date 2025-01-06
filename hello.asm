2434aa50c073f08b69e1392c9190154812f20026
Komus <komolehinisrael@gmail.com>
2025-01-06 12:52:50 +0000

    Added comments

--- hello.asm
+++ hello.asm
@@ -1,16 +1,21 @@
-section .data
-    text db "Hello world", 10
+section .data;  where data is defined before compilation
+    ;   db: define bytes
+    ;   "Hello world": defined bytes
+    ;   Each character in the string is a bytes
+    ;   10: a newline characte
+    ;   text: variable name 
+    text db "Hello, world!", 10
 
-section .text
+section .text;  text section is where the actual code goes
     global _start
 
-_start:
-    mov rax, 1
-    mov rdi, 1
+_start: ; entry point of the program. Essential for all programs
+    mov rax, 1; syscall id (sys_write)
+    mov rdi, 1; id for standard output
     mov rsi, text
     mov rdx, 14
     syscall
 
-    mov rax, 60
-    mov rdi, 0
+    mov rax, 60; syscall (sys_exit)
+    mov rdi, 0; no error code
     syscall
