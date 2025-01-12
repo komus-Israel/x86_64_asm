4aacbbb0a54e763556c936d2d20992c26ba2fd74
Komus <komolehinisrael@gmail.com>
2025-01-12 19:02:59 +0000

    Initialized variables from byte to quadword

--- movement/mov.asm
+++ movement/mov.asm
@@ -12,19 +12,34 @@ section .data  ; <variable name> <type> <value>
 
     ;   ---------
     ;   Byte (8 bits) variable initializations
+    bNum    db  42
+    bAns    db  0
 
-    bVar1   db  17
-    bvar2   db  9
-    bResult db  0
+    ;   ---------
+    ;   Word (16 bits) variable initializations
+    wNum    dw  5000
+    wAns    dw  0
 
-    
-    
-    dValue dd 0; define double word (32 bits)
-    wNum dw 5000; define word (16 bits)
-    
+    ;   ---------
+    ;   Double Word (32 bits) variable initializations
+    dValue  dd  0
+    dNum    dd  73000
+    dAns    dd  0
+
+    ;   ---------
+    ;   Quad Word (64 bits) variable initializations
+    qNum    dq  73000000
+    qAns    dq  0
+
 section .text
     global _start
 
-    
+_start:
+    mov al, byte [bVar1]
+    ; add al, [bVar2]
+    ; mov [bResult], al   
 
-
+last:
+    mov rax, SYS_EXIT
+    mov rdi, EXIT_SUCCESS
+    syscall
