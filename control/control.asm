9162d2c165fa7d873e058e74183e45639ba8a450
Komus <komolehinisrael@gmail.com>
2025-01-14 13:52:27 +0000

    defined data and approach

--- control/control.asm
+++ control/control.asm
@@ -3,11 +3,29 @@
 ;   Komolehin Israel
 ;   -----------------------------
 
+;   Instruction
+;   -----------------------------
+;   Find the sum of squares from 1 to n
+
 section .data
+    ;   ------
+    ;   Define contants
+    EXIT_SUCCESS    equ 0   ;   successful operation
+    SYS_EXIT        equ 60  ;   call code to terminate
+
+    n               dd  10  ;   n is 10 in this case
+    sum_of_squares  dq  0
 
 section .text
     global _start
 
 _start:
+
+    ;   ---------------
+    ;   Approach
+    ;   for (i=1; i<=n; i++)
+    ;       sum_of_squares += i^2
+
+    
 
 last:
