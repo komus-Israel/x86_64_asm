a77e2f9f1cc109081470a009d890d2d607e1238e
Komus <komolehinisrael@gmail.com>
2025-01-14 13:32:46 +0000

    setup file for conditional and loop

---
+++ control/control.asm
@@ -0,0 +1,13 @@
+;   Implement jump and loop
+;   -----------------------------
+;   Komolehin Israel
+;   -----------------------------
+
+section .data
+
+section .text
+    global _start
+
+_start:
+
+last:
