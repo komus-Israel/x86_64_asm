7e4c93a920b4e5fb715f5b22b1f31e728a52169d
Komus <komolehinisrael@gmail.com>
2025-01-25 21:28:41 +0000

    updated comment + reorganized labels

--- assessment/fibonacci.asm
+++ assessment/fibonacci.asm
@@ -13,9 +13,7 @@ section .data
     f_n_1   dq  0;      define (f(n-1)) as quadword (64 bits)
     f_n_2   dq  0;      define (f(n-2)) as quadword (64 bits)
     n       dq  10;      term for the fibonacci sequence where n>=0
-    is_constant db  0;  flag that checks if n is 0 or 1
-
-
+    
 section .text
     global _start
 
@@ -25,24 +23,24 @@ _start:
     je _set_0_or_1
     cmp rax, 1
     je _set_0_or_1
-
+    cmp rax, 2
+    jge _otherwise
     
+
+;   set f_n to be n (f(0) = 0, f(1) = 1)
+;   this is called for the above conditions
+_set_0_or_1:
+    mov qword [f_n], rax
+
+_otherwise:
     mov rcx, [n];     set n as 10, which is destination
     sub rcx, 1;       iteration begins after f(1)
 
     mov rax, 1              ;   set rax for f(n-1)
     mov qword [f_n_1], rax  ;   set f(n-1)
     mov qword [f_n_2], rbx  ;   set f(n-2)
-    
 
-
-;   set f_n to be n (f(0) = 0, f(1) = 1)
-;   this is called for the above conditions
-_set_0_or_1:
-    mov qword [f_n], rax
-    mov al, 1                   ;   update register to update is_constant to true
-    mov byte [is_constant], al  ;   is_constant is true
-
+    jmp _compute_sequence
 
 ;   n >= 2
 ;   f(n) = f(n-1) + f(n-2)
@@ -51,15 +49,16 @@ _set_0_or_1:
 ;   f(2-1) = f(1) = 1
 ;   f(2-2) = f(0) = 0
 ;   where f(n-1) > f(n-2)   
-_otherwise;
+_compute_sequence;
     
     add rax, qword [f_n_2];   f(n-1) + f(n - 2)
     mov qword [f_n], rax
 
-    mov rbx, qword [f_n_1];   update register for new (f(n-2)) value
-    mov qword[f_n_2], rbx ;   update f(n-2) 
-    mov qword[f_n_1], rax ;   update (f(n-1)) value to f(n)
-    loop _otherwise
+    mov rbx, qword [f_n_1]       ;   update register for new f(n-2) value using f(n-1)
+    mov qword[f_n_2], rbx        ;   update f(n-2) 
+    mov qword[f_n_1], rax        ;   update register for new f(n-1) value using f(n)
+    loop _compute_sequence       ;   handle loop counter
+
 
 last:
     mov rax, SYS_EXIT
